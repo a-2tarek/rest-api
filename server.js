b44d23f2836b533677021ba5ae900f0b68c8b12c
a-2tarek <116754953+a-2tarek@users.noreply.github.com>
2023-06-14 14:51:56 +0300

    rest

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express')
 const mongoose = require('mongoose')
 require("dotenv").config()
+const express = require('express')
 
 const app = express()
+
 mongoose.connect(process.env.MONGO_URL)
 
 app.listen(process.env.PORT , () =>
-console.log(`app running om port: ${process.env.PORT}`) )
+console.log(`app running om port: ${process.env.PORT}`))
+
+app.get()
